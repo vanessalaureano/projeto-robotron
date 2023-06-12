639c6c67d71c28da9c46cdf2e43438b0e9314f49
vanessalaureano <solytagamer@gmail.com>
2023-06-12 01:27:19 -0300

    update

---
+++ js/main.js
@@ -0,0 +1,20 @@
+// "function()"" ou "() =>" são formas de escrever funções anônimas
+
+const controle = document.querySelectorAll(".controle-ajuste");
+
+controle.forEach( (elemento) => {
+    elemento.addEventListener("click", (evento) => {
+        manipulaDados(evento.target.textContent, evento.target.parentNode);
+    });
+})
+
+function manipulaDados(operacao, controle) {
+
+    const peca = controle.querySelector(".controle-contador");
+
+    if(operacao === "-") {
+        peca.value = parseInt(peca.value) - 1;
+    } else {
+        peca.value = parseInt(peca.value) + 1;
+    }
+}
